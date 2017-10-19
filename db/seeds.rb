c792a5458665128fdcf02cc13ad3b5c0d49defdf
Miles Heise <milesheise@gmail.com>
2017-10-19 12:51:50 -0400

    possible solution?

--- db/seeds.rb
+++ db/seeds.rb
@@ -17,6 +17,18 @@ posts = Post.all
   )
 end
 
+Post.find_or_create_by(title: 'Unique Post', body: "I'm the only one!")
+
+postid = ''
+posts.each do |x|
+  if x.value?('Unique Post')
+    postid = x.fetch('id')
+    break
+  end
+end
+
+Comment.find_or_create_by(body: 'unique comment', post: postid)
+
 puts 'Seed finished'
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
