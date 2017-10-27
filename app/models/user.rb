1f1b2fcd32cdea1cdd0bb1a06adda6c85dfc8ebf
Miles Heise <milesheise@gmail.com>
2017-10-27 10:12:34 -0400

    refactor

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase if email.present? }
-  before_save { self.name = name.split.map(&:capitalize).join(' ') if name.present? }
+  before_save :downcase_email if: :email_present?
+  before_save :capitalize_name, if: :name_present?
+
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: 'password_digest.nil?'
@@ -11,4 +12,22 @@ class User < ActiveRecord::Base
             length: { minimum: 3, maximum: 254 }
 
   has_secure_password
+
+  private
+
+  def capitalize_name
+    name = name.split.map(&:capitalize).join(' ')
+  end
+
+  def name_present?
+    name.present?
+  end
+
+  def downcase_email
+    email = email.downcase
+  end
+
+  def email_present?
+    email.present?
+  end
 end
