9f125f1a1cdeacf9c2cbd43d67398eafb431eb7a
Miles Heise <milesheise@gmail.com>
2017-10-27 10:57:31 -0400

    new users can be added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ActiveRecord::Base
-  before_save :downcase_email if: :email_present?
+  before_save :downcase_email, if: :email_present?
   before_save :capitalize_name, if: :name_present?
-
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
   validates :password, presence: true, length: { minimum: 6 }, if: 'password_digest.nil?'
