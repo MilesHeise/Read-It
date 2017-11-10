4b7fd9ac080be04b4887024cd36dc8fda26f280a
Miles Heise <milesheise@gmail.com>
2017-11-10 10:08:30 -0500

    profiles added

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
-   end
+  end
 
   def create
     @user = User.new(user_params)
@@ -14,6 +14,11 @@ class UsersController < ApplicationController
       flash.now[:alert] = 'There was an error creating your account. Please try again.'
       render :new
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
+    @posts = @user.posts.visible_to(current_user)
   end
 
   private
